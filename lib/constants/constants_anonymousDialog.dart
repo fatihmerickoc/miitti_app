8d921e4d5fd5163de43076a0f9e39be79d99ff55
Fatih Meric Koc <fatihmerickoc@gmail.com>
2024-04-02 22:43:33 +0300

    anonymous dialog opens properly without errors

--- lib/constants/constants_anonymousDialog.dart
+++ lib/constants/constants_anonymousDialog.dart
@@ -10,59 +10,62 @@ class ConstantsAnonymousDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: 350.h,
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-          color: ConstantStyles.black,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 350.h,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            color: ConstantStyles.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
           ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(10.w),
-          child: Column(
-            children: [
-              const Divider(
-                color: Colors.white,
-                thickness: 2.0,
-                indent: 100,
-                endIndent: 100,
-              ),
-              Text(
-                'Hups!',
-                style: ConstantStyles.title,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 10.w),
-                child: Text(
-                  'Näyttää siltä, ettet ole vielä viimeistellyt profiiliasi, joten et voi käyttää vielä\n sovelluksen kaikkia ominaisuuksia.\n\n Korjataanko asia?',
-                  style: ConstantStyles.body,
+          child: Padding(
+            padding: EdgeInsets.all(10.w),
+            child: Column(
+              children: [
+                const Divider(
+                  color: Colors.white,
+                  thickness: 2.0,
+                  indent: 100,
+                  endIndent: 100,
                 ),
-              ),
-              ConstantsCustomButton(
-                buttonText: 'Viimeistele profiili',
-                onPressed: () {
-                  pushNRemoveUntil(context, const OnboardingScreen());
-                },
-              ), //Removed extra padding in ConstantsCustomButton
-              ConstantStyles().gapH10,
-              ConstantsCustomButton(
-                buttonText: 'Ei vielä',
-                isWhiteButton: true,
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ), //Removed extra padding in ConstantsCustomButton
-              const Spacer(),
-              Text(
-                'Voit myös viimeistellä profiiliasi myöhemmin asetussivulla!',
-                style: ConstantStyles.warning,
-              ),
-            ],
+                Text(
+                  'Hups!',
+                  style: ConstantStyles.title,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w),
+                  child: Text(
+                    'Näyttää siltä, ettet ole vielä viimeistellyt profiiliasi, joten et voi käyttää vielä\n sovelluksen kaikkia ominaisuuksia.\n\n Korjataanko asia?',
+                    style: ConstantStyles.body,
+                  ),
+                ),
+                ConstantsCustomButton(
+                  buttonText: 'Viimeistele profiili',
+                  onPressed: () {
+                    pushNRemoveUntil(context, const OnboardingScreen());
+                  },
+                ), //Removed extra padding in ConstantsCustomButton
+                ConstantStyles().gapH10,
+                ConstantsCustomButton(
+                  buttonText: 'Ei vielä',
+                  isWhiteButton: true,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ), //Removed extra padding in ConstantsCustomButton
+                const Spacer(),
+                Text(
+                  'Voit myös viimeistellä profiiliasi myöhemmin asetussivulla!',
+                  style: ConstantStyles.warning,
+                ),
+              ],
+            ),
           ),
         ),
       ),
