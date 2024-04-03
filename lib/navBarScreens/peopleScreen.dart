11fa63f6d0af75217bc257670f3a081e8c1e1cf1
Samuli Saarinen <samuli.jas@gmail.com>
2024-04-04 01:00:52 +0300

    lockissa outoi juttui

--- lib/navBarScreens/peopleScreen.dart
+++ lib/navBarScreens/peopleScreen.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:math';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
